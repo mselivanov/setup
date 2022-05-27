62685dad2146485520706d3d89f887eb342007ea
Maksim Selivanau <maksim_selivanau@epam.com>
2022-05-27 17:40:40 +0300

    Update windows software list && wsl software
    - Update windows software list
    - Add packages to install in wsl
    -- generic dev dependencies
    -- git
    -- add cleanup section

--- wsl/install.sh
+++ wsl/install.sh
@@ -1,16 +1,35 @@
 #!/usr/bin/env bash
 
+# Install generic development dependencies 
+# ----------------------------------------------------------
+# Update the list of packages
+sudo apt update
+
+sudo apt install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+# Install git 
+# -------------------------
+sudo apt install get
+
 # Install Power Shell
 # -------------------------
-# Update the list of packages
-sudo apt-get update
 # Install pre-requisite packages.
-sudo apt-get install -y wget apt-transport-https software-properties-common
+sudo apt install -y wget apt-transport-https software-properties-common
 # Download the Microsoft repository GPG keys
 wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
 # Register the Microsoft repository GPG keys
 sudo dpkg -i packages-microsoft-prod.deb
 # Update the list of packages after we added packages.microsoft.com
-sudo apt-get update
+sudo apt update
 # Install PowerShell
-sudo apt-get install -y powershell
+sudo apt install -y powershell
+
+# Upgrade packages versions
+# -------------------------
+sudo apt upgrade -y
+
+# Cleanup 
+# -------------------------
+rm "./packages-microsoft-prod.deb"
