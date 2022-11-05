41ae313b19f663028d91397ca9f6004e74861ed5
Maksim Selivanau <maksim_selivanau@epam.com>
2022-11-05 23:34:52 +0200

    Add bash_it installation

--- linux/ubuntu/install.sh
+++ linux/ubuntu/install.sh
@@ -35,6 +35,11 @@ sudo apt install -y make build-essential wget curl git stow neovim podman
 sudo curl -sS https://starship.rs/install.sh | sh -s -- --yes
 eval "$(starship init bash)"
 
+# Install bash_it 
+# -------------------------
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh
+
 # Upgrade packages versions
 # -------------------------
 sudo apt upgrade -y
