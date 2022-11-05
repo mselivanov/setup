1b96b2cba0886927856405b9addae4de82331d5e
Maksim Selivanau <maksim_selivanau@epam.com>
2022-11-05 23:23:48 +0200

    Update install script

--- linux/ubuntu/install.sh
+++ linux/ubuntu/install.sh
@@ -5,16 +5,21 @@
 # Update the list of packages
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install -y make build-essential wget curl 
+sudo apt install -y \
+    make \
+    build-essential \
+    wget \
+    curl \ 
+    git \
+    stow \
+    neovim \
+    podman
 
 # sudo apt install libssl-dev zlib1g-dev \
 # libbz2-dev libreadline-dev libsqlite3-dev \
 # libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \ 
 # libffi-dev liblzma-dev gnupg2 llvm
 
-# Install git 
-# -------------------------
-sudo apt -y install git
 
 # TODO
 # Install python tools
@@ -23,17 +28,13 @@ sudo apt -y install git
 # pipx
 # pipenv
 
-# Install neovim 
-# -------------------------
-sudo apt install -y neovim
-
 # Install podman
 # -------------------------
 # source /etc/os-release
 # sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
 # sudo wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | sudo apt-key add -
 # sudo apt update -qq -y
-sudo apt -qq -y install podman
+# sudo apt -qq -y install podman
 # sudo cp /usr/share/containers/containers.conf /etc/containers/
 # echo "Update configuration in /etc/containers/containers.conf after installation"
 
