b9dca31c4e3db8f7f8308dffa70704a6015e0325
Maksim Selivanau <maksim_selivanau@epam.com>
2022-11-05 23:57:48 +0200

    Add bash_it removal before install

--- linux/ubuntu/install_unprivileged.sh
+++ linux/ubuntu/install_unprivileged.sh
@@ -14,6 +14,7 @@ eval "$(starship init bash)"
 
 # Install bash_it 
 # -------------------------
+rm -rf ~/.bash_it
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh
 
