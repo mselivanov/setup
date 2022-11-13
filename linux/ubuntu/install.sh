34b5335611136ae0c7acff361cb90065174e40a0
Maksim Selivanau <maksim_selivanau@epam.com>
2022-11-14 00:23:35 +0200

    Add neovim repository

--- linux/ubuntu/install.sh
+++ linux/ubuntu/install.sh
@@ -5,7 +5,11 @@
 # Update the list of packages
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install -y make build-essential wget curl git stow neovim podman
+sudo apt install -y make build-essential wget curl git stow podman software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/stable -y
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt update
+sudo apt install -y neovim
 
 # Upgrade packages versions
 # -------------------------
