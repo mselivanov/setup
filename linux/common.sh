506c4fd9e44881285d9b6b4251bdb23dafdfff69
Maksim Selivanau <maksim_selivanau@epam.com>
2022-11-13 23:00:10 +0200

    Fix changing SCRIPT_DIR within sub-script

--- linux/common.sh
+++ linux/common.sh
@@ -1,2 +1,2 @@
-export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source "${SCRIPT_DIR}/common_fs.sh"
+COMMON_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source "${COMMON_SCRIPT_DIR}/common_fs.sh"
