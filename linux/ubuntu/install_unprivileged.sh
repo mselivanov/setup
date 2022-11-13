5a1c5aea0f86fdaf0eae54f3ec48ca2190b0ac07
Maksim Selivanau <maksim_selivanau@epam.com>
2022-11-13 22:55:26 +0200

    Add additional options

--- linux/ubuntu/install_unprivileged.sh
+++ linux/ubuntu/install_unprivileged.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -o nounset    # error when referencing undefined variable
+set -o errexit    # exit when command fails
+
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "${SCRIPT_DIR}/../common.sh"
 
