ac82a182b56f98996a030721e2a37c5de6bdb70b
Maksim Selivanau <maksim_selivanau@epam.com>
2023-05-20 14:13:50 +0300

    feat: add system configuration script

---
+++ linux/arch/configure.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#-----------------------------------------------
+#-----------Time--------------------------------
+#-----------------------------------------------
+#
+# Set local time zone
+sudo ln -sf /usr/share/zoneinfo/Europe/Vilnius /etc/localtime
+# Sync hw clock
+sudo hwclock --systohc
+
+# Configure locale
+sudo sed --in-place=.bak -e 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' -e 's/#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
+sudo locale-gen
+sudo echo "LANG=en_US.UTF-8" >/etc/locale.conf
