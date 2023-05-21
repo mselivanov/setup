92678d897ba999a2d2f010f92e150529a9e9ae18
Maksim Selivanau <maksim_selivanau@epam.com>
2023-05-21 14:12:26 +0300

    feat: change Vagrant file, add python installation

---
+++ linux/arch/python/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+source "${SCRIPT_DIR}/../../common.sh"
+
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+pyenv install 3.11
